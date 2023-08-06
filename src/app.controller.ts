0c9283bd2571262eb574806508a2a40238197ef6
Jonatha Follmer <jonatha.follmer@compasso.com.br>
2023-08-06 14:54:22 -0300

    init prisma service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+// import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
 
-@Controller()
+import { randomUUID } from 'node:crypto';
+
+// inversão de dependências recebendo os paramêtros por um construtor
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prismaService: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  list() {
+    return this.prismaService.notification.findMany();
+  }
+
+  // passando decorators como parâmetros
+  @Post()
+  async create(@Body() body: any) {
+    const { recipientId, content, category } = body;
+    await this.prismaService.notification.create({
+      data: {
+        id: randomUUID(),
+        content: category,
+        category: content,
+        recipientId: recipientId,
+      },
+    });
   }
 }
